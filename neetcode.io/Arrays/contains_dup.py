a4de66c08b090e4d16fbbd622ddf34a9f5d6e3ba
yash Upadhyay <yashupadhyay@yashs-MacBook-Pro.local>
2024-09-25 22:39:00 +0530

    added unit test for Solution class

--- neetcode.io/Arrays/contains_dup.py
+++ neetcode.io/Arrays/contains_dup.py
@@ -1,7 +1,9 @@
 # https://leetcode.com/problems/contains-duplicate/description/
 
+
+
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate(self, nums) -> bool:
         uni = set()
         for i in nums:
             if i in uni:
@@ -9,3 +11,30 @@ class Solution:
             else:
                 uni.add(i)
         return False
+
+import unittest
+
+class TestContainsDuplicate(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_contains_duplicate_true(self):
+        self.assertTrue(self.solution.containsDuplicate([1, 2, 3, 1]))
+
+    def test_contains_duplicate_false(self):
+        self.assertFalse(self.solution.containsDuplicate([1, 2, 3, 4]))
+
+    def test_empty_list(self):
+        self.assertFalse(self.solution.containsDuplicate([]))
+
+    def test_single_element(self):
+        self.assertFalse(self.solution.containsDuplicate([1]))
+
+    def test_multiple_duplicates(self):
+        self.assertTrue(self.solution.containsDuplicate([1, 1, 1, 1]))
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
